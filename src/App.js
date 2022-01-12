018962045d4d606960315748e62394861d36f495
antonio madureira <antonio.madureira2222@gmail.com>
2022-01-11 22:53:49 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <>
+      <h1>WELCOME TO MY PORTFOLIO</h1>
+      </>
+    </div>
+  );
+}
+
+export default App;
