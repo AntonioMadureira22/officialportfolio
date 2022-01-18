9f058729ed6678bca6a1b976d5df68457f7c6c79
antonio madureira <antonio.madureira2222@gmail.com>
2022-01-18 03:21:42 -0500

    worked on the about me section and now onto the portfolio page

--- src/components/Aboutme/about.jsx
+++ src/components/Aboutme/about.jsx
@@ -1,9 +1,24 @@
 import "./about.scss"
+import { Storage, Devices, Code } from '@material-ui/icons';
 
-export default function about() {
+export default function About() {
     return (
         <div className='me' id="about">
-            
+            <div className="left">
+                <h2>Just a,</h2>
+                <h1>Synopsis</h1>
+                <h3>About Me</h3>
+                <p>
+                    I am Software engineer with skills within both front and back end applications. Working my way to the forefront of technology by exploring how it ties with peoples passions. I believe in rapid iteration, continuous improvement, and that good software can make life easier for anyone.  I have an excellent eye for detail and am able to work efficiently. Having a background in the automtive industry, I quickly learned persistence is key. I'm always looking for new challenges to test my capabilities.
+                </p>
+            </div>
+            <div className="right">
+                <div className="itemContainer">
+                    <Storage className="icon" />
+                    <Devices className="icon" />
+                    <Code className="icon" />
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -3,6 +3,17 @@ import "./portfolio.scss"
 export default function portfolio() {
     return (
         <div className="portfolio" id="portfolio">
+            <h1>Portfolio</h1>
+            <ul>
+                <li className="active">Work</li>
+                <li>Content</li>
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="images/react logo.png" alt="" />
+                    <h3>banking</h3>
+                </div>
+            </div>
         </div>
     )
 }
