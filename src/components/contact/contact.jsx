37c409b6b7f9240eedd9e058dabded7c4c203522
antonio madureira <antonio.madureira2222@gmail.com>
2022-01-17 23:11:34 -0500

    intro page styling done

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,9 +1,23 @@
 import "./contact.scss"
+import Link from "@material-ui/core/Link"
+import { GitHub, LinkedIn } from "@material-ui/icons"
 
 export default function contact() {
     return (
         <div className="contact" id="contact">
-            
+            <div className="itemContainer">
+                <Link className="icon"
+                    href="https://github.com/AntonioMadureira22">
+                    <GitHub />
+                </Link>
+                <Link className="icon"
+                    href="https://www.linkedin.com/in/antonio-madureira-a6b806191/">
+                    <LinkedIn />
+                </Link>
+
+
+            </div>
         </div>
+
     )
 }
--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -1,9 +1,36 @@
-import "./intro.scss"
+import "./intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from 'react';
 
-export default function intro() {
+export default function Intro() {
+    const textRef = useRef();
+
+    useEffect(()=>{
+        init(textRef.current, { 
+            backDelay: 1500,
+            backspeed: 50,
+            showCursor: true, 
+            strings: ['Full-stack Developer', 'Content Creator' ] 
+        });
+    
+    }, []);
     return (
         <div className="intro" id="intro">
-            <p className="text">Welcome! My name is Antonio Madureira</p>
+            <div className="left">
+                <div className="imgContainter">
+                    <img src="images/antonio 1 (1).png" alt="person" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                <h2>Welcome to my Portfolio</h2>
+                <h1>My name is, Antonio Madureira</h1>
+                <h3>Software Engineer<span ref={textRef}></span></h3>
+                </div>
+                <a href="#about">
+                <button>More Info</button>
+                </a>
+            </div>
         </div>
     )
 }
