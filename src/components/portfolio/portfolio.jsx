7e0296813c3b5d7c4755b823853d202c832fa1e6
antonio madureira <antonio.madureira2222@gmail.com>
2022-01-18 23:25:54 -0500

    added the feature to selct between toggles for works and content

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,12 +1,34 @@
+import {useEffect, useState } from "react";
+import PortfolioList from "../portfoliolist/PortfolioList";
 import "./portfolio.scss"
 
-export default function portfolio() {
+export default function Portfolio() {
+    const [selected,setSelected] = useState("featured");
+    const list = [
+        {
+            id: "featured",
+            title: "Featured",
+        },
+        {
+            id: "content",
+            title: "Content",
+        },
+    ];
+
     return (
         <div className="portfolio" id="portfolio">
             <h1>Portfolio</h1>
             <ul>
-                <li className="active">Work</li>
-                <li>Content</li>
+                {list.map(item => (
+                    <PortfolioList
+                        title={item.title}
+                        active={selected === item.id}
+                        setSelected={setSelected}
+                        id={item.id}
+                    />
+                ))}
+
+
             </ul>
             <div className="container">
                 <div className="item">
